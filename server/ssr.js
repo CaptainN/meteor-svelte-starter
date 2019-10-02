ad05442baf62d8598bad2a88360670868754728b
Kevin Newman <Kevin@unFocus.com>
2019-10-02 00:51:54 -0400

    Add a lang attribute to the html tag

--- server/ssr.js
+++ server/ssr.js
@@ -1,3 +1,4 @@
+import { WebApp } from 'meteor/webapp'
 import { FastRender } from 'meteor/staringatlights:fast-render'
 import { preloadAll } from 'meteor/npdev:svelte-loadable'
 import App from '/imports/ui/App.svelte'
@@ -5,6 +6,10 @@ import App from '/imports/ui/App.svelte'
 preloadAll().then(() => FastRender.onPageLoad((sink) => {
   const loadableHandle = {}
   const { html, css, head } = App.render({ loadableHandle, url: sink.request.url.pathname })
+
+  WebApp.addHtmlAttributeHook(() => ({
+    lang: 'en'
+  }))
 
   sink.appendToHead(`<style>${css.code}</style>`)
   sink.appendToHead(head)
